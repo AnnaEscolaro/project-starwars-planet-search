7a5a21e25376ef95dcb567f208dbbabbdc5f591c
Anna Escolaro <annaescolaro@hotmail.com>
2023-08-11 16:42:34 -0300

    increasing test coverage

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -4,7 +4,7 @@ import fetchPlanets from '../api/fetch-planets';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
 
-test('São renderizados inputs e botão para adicionar filtro', async () => {
+test('São renderizados inputs, botão para adicionar filtro e dados da API', async () => {
   render(<App />);
   await waitFor(() => expect(screen.getByPlaceholderText(/Nome do Planeta/i)).toBeInTheDocument(), {
     timeout: 6000,
@@ -14,25 +14,27 @@ test('São renderizados inputs e botão para adicionar filtro', async () => {
   const operatorInput = screen.getByDisplayValue(/maior que/i);
   const numberInput = screen.getByDisplayValue(/0/i);
   const button = screen.getByRole('button', { name: /Adicionar Filtro/i });
+  const tableTitle = screen.getByText(/Surface Water/i);
   expect(nameInput).toBeInTheDocument();
   expect(populationInput).toBeInTheDocument();
   expect(operatorInput).toBeInTheDocument();
   expect(numberInput).toBeInTheDocument();
   expect(button).toBeInTheDocument();
   expect(button).not.toBeDisabled();
+  expect(tableTitle).toBeInTheDocument();
 });
 
-// test('I am your test', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/Hello, App!/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
-
-// test('I am your test', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/Hello, App!/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+test('Se os filtros funcionam corretamente', async () => {
+  render(<App />);
+  await waitFor(() => expect(screen.getByPlaceholderText(/Nome do Planeta/i)).toBeInTheDocument(), {
+    timeout: 6000,
+  });
+  await userEvent.type(screen.getByPlaceholderText(/Nome do Planeta/i), 'oo');
+  expect(screen.getByText(/Tatooine/i)).toBeInTheDocument();
+  expect(screen.getByText(/Naboo/i)).toBeInTheDocument();
+  await userEvent.type((screen.getByDisplayValue(/0/i)), '200000');
+  expect(screen.getByText(/Naboo/i)).toBeInTheDocument();
+});
 
 // test('I am your test', () => {
 //   render(<App />);
